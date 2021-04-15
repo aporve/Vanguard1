cadf29a8ba83992bb73fbfe0d682d7ca130f8e2d
Aporve <aporvep@gmail.com>
2021-04-15 19:24:13 +0530

    Small changes

--- js/main.js
+++ js/main.js
@@ -37,26 +37,26 @@ function sendEvent(btnID) {
 }
 
 
-window.addEventListener('message', function(eventData) {
-    let clickedButton = btnID;
-    try { 
-         if(clickedButton){
+// window.addEventListener('message', function(eventData) {
+//     let clickedButton = btnID;
+//     try { 
+//          if(clickedButton){
            
-         }
-        if (JSON.parse(eventData.data)) {
-            let event = JSON.parse(eventData.data);
-             if (event.event_code == "custom-event" || event.data || event.data.code == "ym-client-event" || event.data.code == "clickedButton") {
-                app.triggerIntent('vemo-2021-talking-points-asset-class-return-outlooks')
-            }
-            else if (event.event_code == "custom-event" || event.data || event.data.code == "ym-client-event" ||  event.data.code == "clickedButton"){
-                run_recaptcha();
-                return;
-            } 
-            else{
-                return;
-            }
-         }
-    } catch (error) {
-        return;
-    }
-}, false);
+//          }
+//         if (JSON.parse(eventData.data)) {
+//             let event = JSON.parse(eventData.data);
+//              if (event.event_code == "custom-event" || event.data || event.data.code == "ym-client-event" || event.data.code == "clickedButton") {
+//                 app.triggerIntent('vemo-2021-talking-points-asset-class-return-outlooks')
+//             }
+//             else if (event.event_code == "custom-event" || event.data || event.data.code == "ym-client-event" ||  event.data.code == "clickedButton"){
+//                 run_recaptcha();
+//                 return;
+//             } 
+//             else{
+//                 return;
+//             }
+//          }
+//     } catch (error) {
+//         return;
+//     }
+// }, false);
